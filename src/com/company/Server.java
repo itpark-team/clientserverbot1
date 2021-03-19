ebd2893a251323cac716a53a25a51de3844d9bbf
itpark-team <a.m.smyslov@yandex.ru>
2021-03-19 19:53:25 +0300

    server client

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -28,57 +28,78 @@ public class Server {
             return;
         }
 
-        Socket talking = listener.accept();
-        Log("client is connected");
-
-        DataInputStream in = new DataInputStream(talking.getInputStream());
-        DataOutputStream out = new DataOutputStream(talking.getOutputStream());
-
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss");
-        Date date = new Date();
-
-        Random rand = new Random();
-
         while (true) {
-            String request = in.readUTF();
-            Log("from client: " + request);
 
-            String response = "";
+            Log("server is listening...");
 
-            switch (request) {
-                case "hello":
-                    response = "hi";
-                    break;
-                case "/date":
-                    response = dateFormatter.format(date);
-                    break;
-                case "/time":
-                    response = timeFormatter.format(date);
-                    break;
-                case "/s_name":
-                    response = "127.0.0.1";
-                    break;
-                case "/help":
-                    response = "/date = show current date \n /time = show current time \n /s_name = show server name \n /help = show command list \n /rnd = show random number from 0 to 10";
-                    break;
-                case "/rnd":
-                    response = String.valueOf((int) (Math.random() * (10 - 0 + 1) + 0));
-                    break;
-                case "/gamemode 1":
-                    response = "set own gamemode to creative";
-                    break;
-                case "/gamemode 0":
-                    response = "set own gamemode to survival";
-                    break;
-                default:
-                    response = "unknown message";
-                    break;
+            Socket talking = null;
+            DataInputStream in = null;
+            DataOutputStream out = null;
+
+            boolean isRun = false;
+
+            try {
+                talking = listener.accept();
+                Log("client is connected");
+
+                in = new DataInputStream(talking.getInputStream());
+                out = new DataOutputStream(talking.getOutputStream());
+
+                isRun = true;
+            } catch (Exception e) {
+                Log("client error: " + e.getMessage());
+                continue;
             }
 
-            out.writeUTF(response);
-            Log("to client: " + response);
-            out.flush();
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss");
+            Date date = new Date();
+            Random rand = new Random();
+
+            while (isRun == true) {
+                String request = in.readUTF();
+                Log("from client: " + request);
+
+                String response = "";
+
+                switch (request) {
+                    case "/hello":
+                        response = "hi";
+                        break;
+                    case "/date":
+                        response = dateFormatter.format(date);
+                        break;
+                    case "/time":
+                        response = timeFormatter.format(date);
+                        break;
+                    case "/s_name":
+                        response = "127.0.0.1";
+                        break;
+                    case "/help":
+                        response = "/date = show current date\n/time = show current time \n/s_name = show server name\n/help = show command list\n/rnd = show random number from 0 to 10\n/hello = test command\n/exit = for exit";
+                        break;
+                    case "/rnd":
+                        response = String.valueOf((int) (Math.random() * (10 - 0 + 1) + 0));
+                        break;
+                    case "/gamemode 1":
+                        response = "set own gamemode to creative";
+                        break;
+                    case "/gamemode 0":
+                        response = "set own gamemode to survival";
+                        break;
+                    case "/exit":
+                        isRun=false;
+                        response = "client disconnected";
+                        break;
+                    default:
+                        response = "unknown message";
+                        break;
+                }
+
+                out.writeUTF(response);
+                Log("to client: " + response);
+            }
+            talking.close();
         }
 
     }
